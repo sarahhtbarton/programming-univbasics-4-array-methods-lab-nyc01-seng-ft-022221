c1cf2c8d193c161007369a82e7fe6f9acfed79b1
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 17:45:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,30 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
+puts using_include(['john', 'jacobs', 'jingle'], 'jacobs')
+puts using_include(['john', 'jacobs', 'jingle'], 'sarah')
 
 def using_sort(array)
-
+  return new_array = array.sort
 end
+puts using_sort(['john', 'jacobs', 'jingle'])
 
 def using_reverse(array)
-
+  return new_array = array.reverse
 end
+puts using_reverse(['john', 'jacobs', 'jingle'])
 
 def using_first(array)
-
+  return array.first
 end
+puts using_first(['john', 'jacobs', 'jingle'])
 
 def using_last(array)
-
+  return array.last
 end
+puts using_last(['john', 'jacobs', 'jingle'])
 
 def using_size(array)
-
+  return array.size
 end
+puts using_size(['john', 'jacobs', 'jingle'])
